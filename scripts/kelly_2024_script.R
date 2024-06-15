4901d46b876d7119f4437002beccbb9005ae4dcf
Kelly Clint Dale <kellyclintdale@C58729.local>
2024-06-15 12:11:47 -0400

    Updates.

--- scripts/kelly_2024_script.R
+++ scripts/kelly_2024_script.R
@@ -59,6 +59,91 @@ check_brms <- function(model,             # brms model
   invisible(dharma.obj)
   
 }
+
+# Load Functions
+
+# Load in the functions that will be needed to process output from the models to derive the various estimates needed for calculating the necessary parameters:
+  
+# {r funcs, include=TRUE, message=FALSE, warning=FALSE, class.source='klippy'}
+
+# Population-level intercept   
+func_Bp <- function(B0, B1){
+  Bp <- (2*B0 + B1)/2 # assumes 50/50 ratio for B1 (in our example = sex)
+  return(Bp)
+}
+
+# Total variance, equation 40. Function for summing dataframe of variance components 
+func_sum_var <- function(vars){
+  sum_var <- sapply(1:nrow(vars), function(x) sum(vars[x,], na.rm = T))
+  return(sum_var)
+}
+
+
+# Variance for fixed effects
+func_var_fixed <- function(B1, B2, data){
+  # model matrix for fixed effects
+  X <- model.matrix(~ 1 + sex.centred + sex.centred:observation.n , data = data) 
+  X1 <- X[,2]
+  X2 <- X[,3]
+  var_fixed <- sapply(1:length(B1), function(x) var(X1*B1[x] + X2*B2[x]))
+  return(var_fixed)
+}		
+
+# within-individual standard deviation
+func_var_within <- function(Bpv, sigma_v0, var_fixed_v){
+  
+  # summed variance components
+  var_within_exp <- func_sum_var(vars = data.frame(sigma_v0^2, var_fixed_v))
+  # conversion back from log scale
+  var_within <- func_ln_convert(mu_ln = Bpv, sigma_ln = sqrt(var_within_exp))$mu_raw
+  
+  return(var_within)
+}
+
+# Repeatability for the dispersion model for DHGLM    
+func_Rp_var <- function(Bpv, sigma_v0, var_fixed_v, var_p){
+  
+  # sum of IDv0 and fixedV = total variance in residual variance on log-normal scale:
+  var_residvar_exp <- func_sum_var(vars = data.frame(sigma_v0^2, var_fixed_v))
+  # converting back to same scale as mean model: 
+  sigma_residvar <- func_ln_convert(mu_ln = Bpv, sigma_ln = sqrt(var_residvar_exp))$sigma_raw
+  
+  # variance in phenotypic variance
+  total_var_var <- 2*var_p^2 + 3*sigma_residvar^2
+  
+  # getting variance in individual component through the preservation of the proportionality (i.e. ratio method)
+  ratio <- sigma_v0^2/(var_residvar_exp)
+  var_ID <- sigma_residvar^2*ratio
+  
+  Rp_var <- var_ID/total_var_var
+  return(Rp_var)
+}
+
+# Convert from ln to raw scale
+func_ln_convert <- function(mu_ln, sigma_ln){
+  
+  mu_raw <- exp(mu_ln + sigma_ln^2/2)
+  var_raw <- (exp(sigma_ln^2)-1)*exp(2*mu_ln + sigma_ln^2)
+  
+  x <- data.frame(mu_raw, sigma_raw = sqrt(var_raw))
+  return(x)
+}	 
+
+
+# Function takes random slopes and intercepts and calculates the between individual correlation between them and returns this correlation for each row (i.e., posterior sampling iteration). It returns a posterior distribution of the correlation. 
+
+cor_calc <- function(slopes, intercepts){
+  cors <- c()
+  for(i in 1:dim(intercepts)[1]){
+    cors <- c(cors, cor(as.numeric(slopes[i,]),as.numeric(intercepts[i,])))
+  }
+  return(cors)
+}
+
+
+
+
+
 ## ---- data_analysis ----
 mobility_data<- read.csv(file="data/raw/giant_weta_behaviour.csv", header=TRUE, sep=",", dec=".")
 morphological_data<- read.csv(file="data/raw/morphology_giant_weta.csv", header=TRUE, sep=",", dec=".")
@@ -92,11 +177,13 @@ morphology_long$sex.centred <- ifelse(morphology_long$sex == 'f', -0.5,
                                    ifelse(morphology_long$sex == 'm', 0.5, NA))
 
 # Transform response variable
+distanceBN<-bestNormalize(morphology_long$distance)
+morphology_long$distance.tr <- distanceBN$x.t
 
-morphology_long$distance.t <- log(distance+1)
+#morphology_long$distance.t <- log(distance+1)
 
 # Scale response variable
-morphology_long$distance.z <- scale(as.numeric(morphology_long$distance.t))
+morphology_long$distance.z <- scale(as.numeric(morphology_long$distance.tr))
 morphology_long$smi.z <- scale(as.numeric(morphology_long$smi))
 
 # Add mating success
@@ -117,11 +204,11 @@ fit_model.brms.activity.1 = readRDS(file = "data/processed/fit_model.brms.activi
 
 #double_model = bf(distance ~ sex + (sex|ID), sigma ~ (1|ID))
 
-mobility_rep <- bf(distance.z ~ sex.centred + sex.centred*observation.n + (observation.n|ID), family = gaussian)
+mobility_rep <- bf(distance.z ~ sex.centred + observation.n + sex.centred*observation.n + (observation.n|ID), family = gaussian)
 
 fit.model.brms.mobility <- brm(mobility_rep, data = morphology_long, save_pars = save_pars(all = TRUE), 
-                             warmup=1000, iter=8000, seed=12345, thin=2, chains=4, cores= 4)
-summary(fit.model.brms.mobility,prob = 0.89)
+                             warmup=100, iter=2000, seed=12345, thin=2, chains=4, cores= 4)
+summary(fit.model.brms.mobility,prob = 0.95)
 
 # males travel farther than females as shown elsewhere
 # suggests behavioural plasticity i.e. sd(observation.n)
@@ -163,14 +250,17 @@ summary(fit.model.brms.mating, prob = 0.95) # no effect of mating success on dis
 ggplot(males, aes(x=distance.z, y=ms)) +
   geom_point()
 
-
 #### PREDICTABILITY ####
 
-mobility_pred <- bf(distance.z~ sex.centred + sex.centred*observation.n + (observation.n|ID), sigma ~ sex, family = gaussian)
+mobility_pred <- bf(distance.tr ~ sex.centred + observation.n + sex.centred:observation.n + (observation.n|ID), sigma ~ sex , family = gaussian)
 
 fit.model.brms.pred <- brm(mobility_pred, data = morphology_long, save_pars = save_pars(all = TRUE), 
-                               warmup=1000, iter=8000, seed=12345, thin=2, chains=4, cores= 4)
+                               warmup=6000, iter=10000, seed=12345, thin=4, chains=4, cores= 4, file = 'data/processed/fit.model.brms.pred')
 summary(fit.model.brms.pred, prob = 0.95)
+
+plot(fit.model.brms.pred, ask = F)
+brms::pp_check(fit.model.brms.pred, resp = "distance.z", ndraws = 50)
+conditional_effects(fit.model.brms.pred, prob = 0.95)
 
 #plot
 
@@ -200,6 +290,138 @@ ggplot(mobility.residual, aes(x = Estimate, fill = Sex)) +
 
 # males less predictable than females
 # correlate rIIV with mating success = less predictable males have higher mating success
+
+get_variables(fit.model.brms.pred)
+#################################
+# Mean and Dispersion Parameters
+#################################
+
+# (Step 1): Extract the population coefficients for mean model
+B0_m <- brms::as_draws_matrix(fit.model.brms.pred, variable = "b_Intercept")	 
+B1_m <- brms::as_draws_matrix(fit.model.brms.pred, variable = "b_sex.centred")
+B2_m <- brms::as_draws_matrix(fit.model.brms.pred, variable = "b_sex.centred:observation.n")
+
+mean(B0_m)
+
+mean(B1_m)
+
+mean(B2_m)
+
+# (Step 2): Extract the population coefficients for dispersion model	 
+B0_sd_exp <- brms::as_draws_matrix(fit.model.brms.pred, variable = "b_sigma_Intercept")	
+B1_sd_exp <- brms::as_draws_matrix(fit.model.brms.pred, variable = "b_sigma_sexm")
+#B2_sd_exp <- brms::as_draws_matrix(fit.model.brms.pred, variable = "b_sigma_age.Z")
+
+mean(2*B0_sd_exp)
+
+mean(2*B1_sd_exp)
+
+#mean(2*B2_sd_exp)
+
+# (Step 3): Extract the random ID intercept variance components for random intercept. Note that we model ln(sd) so we need to convert to variance scale by multipling by 2.
+sigma_ID_m0 <- brms::as_draws_matrix(fit.model.brms.pred, variable = "sd_ID__Intercept")
+
+mean(sigma_ID_m0)
+
+sigma_ID_sd0 <- brms::as_draws_matrix(fit.model.brms.pred, variable = "sd_ID__observation.n")
+sigma_ID_v0 <- 2*sigma_ID_sd0
+
+mean(sigma_ID_v0)
+
+# (Step 4): **New step as we now have a sd for the random slopes.**
+sigma_ID_m2 <- brms::posterior_samples(brms_personality_plasticity_dhglm_aggression, pars = "sd_id__age.Z")[,1]
+
+mean(sigma_ID_m2)
+
+# (Step 5): Calculate the fixed effect variance for the mean model (var_fixed_m) and the dispersion model (B_pv). Note here that the calculations are the same because we assume intercept at an average age.
+B_pm <- func_Bp(B0 = B0_m, B1 = B1_m)
+B_pv <- 2*func_Bp(B0 = B0_sd_exp, B1 = B1_sd_exp)
+
+mean(B_pm)
+
+mean(B_pv)
+
+# (Step 6): Calculate the population intercept for the mean model (var_fixed_m) and dispersion model (var_fixed_v). Note that a variance on the ln(sd) scale can be converted to a ln(var) by multipling by 4. See supplemental text.
+var_fixed_m <- func_var_fixed(B1 = B1_m, B2 = B2_m, data = morphology_long)
+var_fixed_v <- 4*func_var_fixed(B1 = B1_sd_exp, data = morphology_long)
+
+mean(var_fixed_m)
+mean(stanproc_personality_plasticity_dhglm_aggression$var1_fixed_m)
+
+mean(var_fixed_v)
+mean(stanproc_personality_plasticity_dhglm_aggression$var1_fixed_v)
+
+
+
+###################################
+# Mean Model - Repeatability and CV
+###################################
+
+# (Step 1): Calculate average within-individual / residual variance
+sigma_w <- func_var_within(Bpv = B_pv, 
+                           sigma_v0 = sigma_ID_v0, 
+                           var_fixed_v = var_fixed_v)
+
+mean(sigma_w)
+mean(stanproc_personality_dhglm_aggression$sigma1_w)
+
+# (Step 2): Calculate total phenotypic variance 
+var_p <- func_sum_var(vars = data.frame(sigma_ID_m0^2, var_fixed_m, sigma_w^2))
+
+mean(var_p)
+mean(stanproc_personality_dhglm_aggression$var1_p)
+
+# (Step 3): Calculate repeatability
+Rp_mu <- sigma_ID_m0^2/var_p
+
+mean(Rp_mu)
+mean(stanproc_personality_dhglm_aggression$Rp1_mu)
+
+# (Step 4): Convert back from z-scale to calculate CVm
+mu_unscaled <- mean(dat$stim.dist_0_5cm_dur_Aggression)
+sigma_unscaled <-   sd(dat$stim.dist_0_5cm_dur_Aggression)
+
+Mu <- B_pm*sigma_unscaled + mu_unscaled
+Sigma <- sqrt(var_p)*sigma_unscaled
+indiv_sd <- sigma_ID_m0*sigma_unscaled
+
+# (Step 5): Calculate CVm for mean model
+CV_mu <- indiv_sd/Mu
+
+mean(CV_mu)
+mean(stanproc_personality_dhglm_aggression$CV1_mu)
+
+###################################
+# Dispersion Model - Repeatability and CV
+###################################
+
+# (Step 1): Calculate repeatability for the dispersion model
+Rp_var <- func_Rp_var(Bpv = B_pv, 
+                      sigma_v0 = sigma_ID_v0, 
+                      var_fixed_v = var_fixed_v, 
+                      var_p = var_p)
+
+mean(Rp_var)
+mean(stanproc_personality_dhglm_aggression$Rp1_var)
+
+# (Step 2): Sum of IDv0 and fixedV = total variance in residual variance on log-normal scale
+var_residvar_exp <- func_sum_var(vars = data.frame(sigma_ID_v0^2, var_fixed_v))
+
+
+# (Step 3): Converting back to same scale as mean model  
+sigma_residvar <- func_ln_convert(mu_ln = B_pv, 
+                                  sigma_ln = sqrt(var_residvar_exp))$sigma_raw
+
+# (Step 4): Getting variance in individual component through the preservation of the proportionality (i.e. ratio method)
+ratio <- sigma_ID_v0^2/(var_residvar_exp)
+var_ID <- sigma_residvar^2*ratio
+
+# (Step 5): Calculate coefficient of variation 
+CV_var <- sqrt(var_ID)/sigma_w^2   
+
+mean(CV_var)
+mean(stanproc_personality_dhglm_aggression$CV1_var)
+
 
 
 #####
